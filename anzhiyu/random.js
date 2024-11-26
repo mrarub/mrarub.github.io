94d1f4431fc94dd2f7fb624b11f8043fb43f40e9
tlb-boy <2477512002@qq.com>
2024-11-26 20:47:55 +0800

    Site updated: 2024-11-26 20:47:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/e118.html","posts/3eeb.html"];function toRandomPost(){
+var posts=["posts/3eeb.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
